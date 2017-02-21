418a05f4cbfeeb43f18ba82acf3f4d80be0dcc13
Matteo Cajani <matteo.cajani@i-faber.com>
2017-02-21 22:30:13 +0000

    initial import

---
+++ src/test/java/it/cajani/matteo/salestaxes/SalesTaxesExerciseApplicationTests.java
@@ -0,0 +1,16 @@
+package it.cajani.matteo.salestaxes;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SalesTaxesExerciseApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
